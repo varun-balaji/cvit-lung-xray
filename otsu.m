f238eb804989b5d733a813408e03de0a9d72b5ac
varunbalaji <varunbalaji40@gmail.com>
2019-06-28 15:34:53 +0530

    removed gaussian filter from otsu.m

--- otsu.m
+++ otsu.m
@@ -2,8 +2,9 @@ clear;
 
 img = rgb2gray(imread("normal-chest-x-ray.jpg"));
 
-img_gauss = imgaussfilt(img, 2);
-otsu_threshold = graythresh(img_gauss);
-img_otsu = imbinarize(img_gauss, otsu_threshold);
+img_gauss = imgaussfilt(img, 1);
+
+otsu_threshold = graythresh(img);
+img_otsu = imbinarize(img, otsu_threshold);
 
 imshow(img_otsu, []);
