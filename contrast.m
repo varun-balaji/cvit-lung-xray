9777fba1998413f421726d26f1a9047e9363ee29
varunbalaji <varunbalaji40@gmail.com>
2019-06-28 14:57:16 +0530

    added otsu method

--- contrast.m
+++ contrast.m
@@ -4,6 +4,7 @@ img = rgb2gray(imread("normal-chest-x-ray.jpg"));
 
 img_gauss = imgaussfilt(img, 2);
 
-
 t = 168;
-imshow(img_gauss - (t - img_gauss));
+img_contrasted = img_gauss - (t - img_gauss);
+
+imshow(img_contrasted);
